65fab8cb537486c216ce00f93981a40903075f8a
Sara-mwk <106980251+Sara-mwk@users.noreply.github.com>
2022-08-16 13:15:46 -0500

    Added search engine

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ function formatDate(timestamp) {
     }
     
     
+    
     function displayTemperature(response) {
         let temperatureElement = document.querySelector("#temperature");
         let cityElement = document.querySelector("#city");
@@ -39,9 +40,24 @@ function formatDate(timestamp) {
         iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
         iconElement.setAttribute("alt", response.data.weather[0].description);
     }
+    
+    function search(city) {
+        let apiKey = "8cfa4dd3ccafa97ae01716474ab8d486";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=$
+        {apiKey}&units=metric`;
+        axios.get(apiUrl).then(displayTemperature);
 
-let apiKey = "8cfa4dd3ccafa97ae01716474ab8d486";
-let city = "Omaha";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    }
 
-axios.get(apiUrl).then(displayTemperature);
+    function handleSubmit(event) {
+        event.preventDefault();
+        let cityInputElement = document.querySelector("#city-input");
+        search(cityInputElement.value);
+      }
+      
+
+    search("Omaha");
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
